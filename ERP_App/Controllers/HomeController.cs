44f3720fe8d261e9c98f67fc163222351c6a98c7
Khawar <khawargsabir@gmail.com>
2022-01-12 09:05:14 +0500

    Admin Login Completed

--- ERP_App/Controllers/HomeController.cs
+++ ERP_App/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ERP.DatabaseLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,30 @@ namespace ERP_App.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Login()
+        public ActionResult Login(string UserEmail, string Password)
         {
+            if(!string.IsNullOrEmpty(UserEmail) && !string.IsNullOrEmpty(Password))
+            {
+                using (BusinessERPSystemEntities db = new BusinessERPSystemEntities())
+                {
+                    var user = db.tblUsers.Where(u => u.Email == UserEmail && u.Password == Password).FirstOrDefault();
+                    if(user == null)
+                    {
+                        ViewBag.ErrorMsg = "Email or Passsword is incorrect";
+                    }
+                    else
+                    {
+                        if(user.UserTypeID ==1)
+                        {
+                            return RedirectToAction("Dashboard", "Admin");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                ViewBag.ErrorMsg = "Please Fill the Email and Password";
+            }
             return View();
         }
     }
